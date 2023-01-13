705b4f9acff27d20cada8b1d9dda704af74e8f82
LouiseX-10 <louisexu426@gmail.com>
2023-01-13 11:48:59 -0800

    player sets up own listeners

--- poker/main.js
+++ poker/main.js
@@ -15,21 +15,24 @@ class Player {
         this.tokenTable = document.querySelector('.player .table.tokens');
         this.commonTable = document.querySelector('.common .table.cards');
         this.commonTokenTable = document.querySelector('.common .table.tokens');
+
+        this.betBtn = document.querySelector('.player button.bet');
+        this.foldBtn = document.querySelector('.player button.fold');
+        
         this.folded = false;
+        this.playerCards = [null, null]; // [card1, card2]
+        this.commonCards = [];
 
-        this.cards = [null, null]; // [card1, card2]
-        this.btns = [null, null, null]; // [nextBtn, betBtn, foldBtn]
-
-        this.money = 0;
-        this.tokens = {}; // dictionary {value : count}
-
-        this.betValue = 0;
+        this.money = 0; // total value of tokens this player owns
+        this.tokens = {}; // token frequency dictionary {value : count}
+        this.betValue = 0; // current bet value in one round
 
         this.handCards = []; // in ascending order after evaluate
         this.handName = '';
         this.handRank = null;
         this.rankCards = [];
         this.highCards = [];
+        
         this.setTokens();
 
         // Get all defined class methods
@@ -41,23 +44,24 @@ class Player {
             .forEach((method) => { this[method] = this[method].bind(this); });
     }
 
+    // Display common cards
     setCommonCards (commonCards){
         console.log('start setting');
+        this.commonCards = commonCards;
         commonCards.forEach(cardval=>{
-            // create new card object and display
-            let card = new Card(cardval[0], cardval[1]);
-            console.log(card);
+            let card = new Card(cardval[0], cardval[1]); // create new card object from given values
             card.displayCard(this.commonTable);
         });
     }
     
 
-    setCards(card1, card2){
-        this.cards = [card1, card2];
-    }
-
-    setButtons(nextBtn, betBtn, foldBtn){
-        this.btns = [nextBtn, betBtn, foldBtn];
+    // Display player cards, setup cardflip listener
+    setPlayerCards(card1, card2){
+        this.playerCards = [card1, card2];
+        this.playerCards.forEach(card => {
+            card.displayCard(this.playTable);
+            card.container.addEventListener('click', this.flipListener)
+        });
     }
 
     // Set tokens to totalvalue
@@ -74,12 +78,33 @@ class Player {
         }
     }
 
-    // (Create and) Place token of given value into table
+    selectListener(event){
+        let token = event.target;
+        token.classList.toggle("selected");
+    }
+    
+    flipListener(event) {
+        let card = event.currentTarget;
+        card.classList.toggle('flip');
+    }
+
+    // Create token and return
+    createToken(value){
+        const token = document.createElement('img');
+        token.src = tokenPath + `token_${value}.svg`
+        token.alt = "token";
+        token.classList.add("token", `_${value}`, "player");
+        return token;
+    }
+
+    // Create and Place token of given value into table, Update token frequency dict
+    // Setup token select listener
     addToken(value){
-        let token = createToken(value);
+        let token = this.createToken(value);
         this.tokenTable.appendChild(token);
         this.money += value;
         this.tokens[value]? this.tokens[value] ++ : this.tokens[value] = 1;
+        token.addEventListener('click', this.selectListener);
     }
 
     // clear all tokens on player table
@@ -91,7 +116,8 @@ class Player {
     }
 
     // move given token to player table
-    collectToken(token){
+    //! Change collect mechanism to add tokens
+    /* collectToken(token){
         let cloneToken = token.cloneNode();
         cloneToken.classList.add('hidden');
         let xi = token.getBoundingClientRect()['x'];
@@ -103,10 +129,10 @@ class Player {
         token.style.transform = `translate(${xf-xi}px, ${yf-yi}px)`;
         setTimeout(()=>{this.tokenTable.appendChild(token);
             token.style.transform = '';}, 1000);
-    }
+    } */
 
     // move given token to common table
-    moveToken(token){
+    /* moveToken(token){
         token.classList.remove('selected');
         let cloneToken = token.cloneNode();
         cloneToken.classList.add('hidden');
@@ -119,10 +145,11 @@ class Player {
         token.style.transform = `translate(${xf-xi}px, ${yf-yi}px)`;
         setTimeout(()=>{this.commonTable.appendChild(token);
             token.style.transform = '';}, 1000);
-    }
+    } */
 
     // Make bet and update highest bet, return whether bet was successful
-    makeBet() {
+    //! Change make bet to remove selected tokens and update bet values
+    /* makeBet() {
         let success = true;
         console.log('!running make bet')
         console.log(this)
@@ -159,12 +186,12 @@ class Player {
             if (card.isFlipped()){card.flip()};
         })
         return success;
-    }
+    } */
 
     makeFold(){
         this.folded = true;
         // Flip back any flipped cards
-        this.cards.forEach((card)=>{
+        this.playerCards.forEach((card)=>{
             if (card.container.classList.contains('flip')){card.flip()};
         })
         // Unselect any selected tokens
@@ -175,10 +202,10 @@ class Player {
         // Increment number of players folded in the game
         game.foldedCount ++;
         // Remove token and card listeners
-        let tokens = this.tokenTable.querySelectorAll('.token');
+        //! Don't need to remove listener for folded players, they just wont get interact with server anymore
+        /* let tokens = this.tokenTable.querySelectorAll('.token');
         tokens.forEach(token=>token.removeEventListener('click', this.selectListener))
-        let cards = this.playTable.querySelectorAll('.card');
-        cards.forEach(container => container.removeEventListener('click', this.flipListener));
+        this.playerCards.forEach(card => card.container.removeEventListener('click', this.flipListener)); */
     }
 }
 
@@ -205,6 +232,7 @@ class Card {
         }
     }
 
+    // Create card container and display on given table
     displayCard (table) {
         // Create card syntax
         this.container = document.createElement('div');
@@ -231,6 +259,7 @@ class Card {
         this.table = table;
     }
 
+    // remove card from table
     removeCard(){
         this.table.removeChild(this.container);
     }
@@ -246,14 +275,13 @@ class Card {
 
 const player = new Player();
 console.log('player is', player)
-//player.firstPlayer = true;
-
-const nextBtn = document.querySelector('button.next');
-const resetBtn = document.querySelector('button.reset');
-const betBtn = document.querySelector('.player button.bet');
-const foldBtn = document.querySelector('.player button.fold');
-player.setButtons(nextBtn, betBtn, foldBtn);
 console.log('main js complete')
+
+//! controlled by server
+// const nextBtn = document.querySelector('button.next');
+// const resetBtn = document.querySelector('button.reset');
+//player.setButtons(nextBtn, betBtn, foldBtn);
+
 
 //! in server
 // players = [player1, player2]
